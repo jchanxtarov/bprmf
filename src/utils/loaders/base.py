4119cbf39f324e95580906f6400c5dd0afa40b7a
jchanxtarov <ryotaro.shimizu@zozo.com>
2021-10-13 11:28:31 +0900

    [fix] _load_cf_txt & _load_cf_dict

--- src/utils/loaders/base.py
+++ src/utils/loaders/base.py
@@ -22,8 +22,11 @@ class BaseDataset(metaclass=ABCMeta):
 
     def __init__(self, dataset: str) -> None:
         # read files
-        self.arr_train_pos, self.dict_train_pos = self._load_pos(f"./src/datasets/{dataset}/train_user_items.pkl")
-        self.arr_test_pos, self.dict_test_pos = self._load_pos(f"./src/datasets/{dataset}/test_user_items.pkl")
+        path_train_cf = f"./src/datasets/{dataset}/train.txt"
+        path_test_cf = f"./src/datasets/{dataset}/test.txt"
+
+        self.arr_train_pos, self.dict_train_pos = self._load_cf_txt(path_train_cf)
+        self.arr_test_pos, self.dict_test_pos = self._load_cf_txt(path_test_cf)
         self.n_train = len(self.arr_train_pos)
         self.n_test = len(self.arr_test_pos)
 
@@ -32,11 +35,31 @@ class BaseDataset(metaclass=ABCMeta):
         self.n_users = max(self.uniq_users) + 1
         self.n_items = max(max(self.arr_train_pos[:, 1]), max(self.arr_test_pos[:, 1])) + 1
 
-    def _load_pos(self, path_dataset: str) -> Tuple[np.ndarray, UserItems]:
+    def _load_cf_txt(self, path_dataset: str) -> Tuple[np.ndarray, UserItems]:
         with open(path_dataset, mode='rb') as f:
             dict_uid_iids = pickle.load(f)
         list_uid_iid = [[uid, iid] for (uid, iids) in dict_uid_iids.items() for iid in list(set(iids))]
         return np.array(list_uid_iid), dict_uid_iids
+
+    def _load_cf_dict(self, path_dataset: str) -> Tuple[np.ndarray, UserItems]:
+        dict_uid_iids = dict()
+        list_uid_iid = list()
+
+        lines = open(path_dataset, "r").readlines()
+        for line in lines:
+            tmps = line.strip()
+            inters = [int(i) for i in tmps.split(" ")]
+
+            userid, itemids = inters[0], inters[1:]
+            itemids = list(set(itemids))
+
+            for itemid in itemids:
+                list_uid_iid.append([userid, itemid])
+
+            if len(itemids) > 0:
+                dict_uid_iids[userid] = itemids
+        return np.array(list_uid_iid), dict_uid_iids
+
 
     @abstractmethod
     def logging_statistics(self) -> None:
